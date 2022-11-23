1c5c40becc9fad79f004ef07a180d46551d7259a
Mitsie01 <mitch.kampert@gmail.com>
2022-11-23 11:19:33 +0100

    switched to object model

--- body.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-mass = 5.9722*10**24
-radius = 6.3781*10**6
-mass_center = np.array([0, 0])
--- main.py
+++ main.py
@@ -1,90 +1,66 @@
 import math
-import numpy as np
-import scipy.constants
-import scipy.integrate
-import matplotlib as plot
+from itertools import count
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
-import body
-import satellite
+class Body:
+    def __init__(self, name, mass, radius, velocity, direction, x, y):
+        self.name = name
+        self.mass = mass
+        self.radius = radius
+        self.vx = velocity*math.cos(direction)
+        self.vy = velocity*math.sin(direction)
+        self.x = x
+        self.y = y
 
-# General constants
-G = scipy.constants.G
-
-dt = 0.1
-tmin = 0
-tmax = 100
-
-# Initial conditions
-velocity = 7700
-altitude = 400*10**3
-pitch = 0
-longitude = 0
-
-
-# Forces
-def gravity(G, M1, M2, r):
-    Fgrav = G*((M1*M2)/r**2)
-    return Fgrav
+    def update(self, F, dt):
+        ax = F[0]/self.mass
+        ay = F[1]/self.mass
+        self.vx += (ax*dt)
+        self.vy += (ay*dt)
+        self.x += (self.vx*dt)
+        self.y += (self.vy*dt)
 
 
-# Initial condition convertion
-r = body.radius + altitude
-pitch = math.radians(pitch)
-longitude = math.radians(longitude)
-x0 = math.cos(longitude)*r
-y0 = math.sin(longitude)*r
-vx0 = math.cos(longitude+90-pitch)*velocity
-vy0 = math.sin(longitude+90-pitch)*velocity
-
-r_mag = math.sqrt(r)
-r_x = x0/r_mag
-r_y = y0/r_mag
-
-ax0 = (gravity(G, body.mass, satellite.mass, r)/satellite.mass)*-r_x
-ay0 = (gravity(G, body.mass, satellite.mass, r)/satellite.mass)*-r_y
+bodies = {
+    0: Body("Earth", 5.97217*10**24, 6371*10**3, 0, 0, 0, 0),
+    1: Body("Moon", 7.342*10**22, 1737.4*10**3, 1022, math.pi/2, 390_000*10**3, 0)
+}
 
 
-# Initialize arrays
-B1x = np.array([[x0, y0, tmin]])
-B1v = np.array([[vx0, vy0, tmin]])
-B1a = np.array([[ax0, ay0, tmin]])
+def Fgrav(mass_self, x_self, y_self, mass2, x2, y2):
+    G = 6.67408*10**-11
+    r = ((x_self-x2)**2 + (y_self-y2)**2)**0.5
+    Fg = (G*mass_self*mass2)/(r**2)
+
+    Fgx = ((x2-x_self)/r)*Fg
+    Fgy = ((y2-y_self)/r)*Fg
+
+    Fg = [Fgx, Fgy]
+
+    return Fg
 
 
-# Start simulation
-for t in np.arange(tmin, tmax, dt):
-    timestamp = int(t*10)
-    print(timestamp)
-    print(B1x.item((timestamp,0)))
-    print(type(B1x.item(timestamp,0)))
 
+dt = 10000
+
+
+for t in range(0, 500):
     
-    r_mag = math.sqrt((B1x.item((timestamp, 0))**2)+(B1x.item((timestamp, 1))**2))
-    r_x = B1x.item((timestamp, 0))/r_mag
-    r_y = B1x.item((timestamp, 1))/r_mag
+    # Calculate gravitational force applied on each body by all bodies
+    for body in bodies.values():
+        Fg = [0, 0]
+        for i in bodies.values():
+            if body != i:
+                Fgs = Fgrav(body.mass, body.x, body.y, i.mass, i.x, i.y)
+                Fg[0] += Fgs[0]
+                Fg[1] += Fgs[1]
 
-    Fgrav = gravity(G, body.mass, satellite.mass, r)
-    Fgravx = Fgrav * -r_x
-    Fgravy = Fgrav * -r_y
+        # Calculate sum of forces on body
+        F = Fg
 
-    Ftotx = Fgravx
-    Ftoty = Fgravy
+        # Update forces on body
+        body.update(F, dt)
+        plt.scatter(body.x, body.y)
 
-    a = np.array([Ftotx/satellite.mass, Ftoty/satellite.mass, t])
-
-    B1a = np.r_[B1a,[a]]
-
-    vx = scipy.integrate.cumulative_trapezoid(B1a[:,0], B1a[:,2], initial=0) + vx0
-    vy = scipy.integrate.cumulative_trapezoid(B1a[:,1], B1a[:,2], initial=0) + vy0
-
-    v = np.array([vx, vy, t])
-
-    B1v = np.r_[B1v,[v]]
-
-    xx = scipy.integrate.cumulative_trapezoid(B1v[:,0], B1v[:,2], initial=0) + x0
-    xy = scipy.integrate.cumulative_trapezoid(B1v[:,1], B1v[:,2], initial=0) + y0
-
-    x = np.array([xx, xy, t])
-
-    B1x= np.r_[B1x,[x]]
-
-    print(B1x.item((1,0)))
+plt.show()
